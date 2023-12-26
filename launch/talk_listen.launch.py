39592b220ff2c684c414ce1016dc69c111b67a0d
Soshi Saiki <sousaiky@yahoo.com>
2023-12-26 22:21:59 +0900

    add command

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
 #!/user/bin/python3
-=======
 #SPDX-FileCopyrightText: 2023 Soshi Saiki
 #SPDX-Licence-Identifire: BSD-3-Clause
->>>>>>> ef89fdbf88fdb635bed7a5484a5fe3ac359f89c0
 
 import launch
 import launch.actions
 import launch.substitutions
 import launch_ros.actions
-import time
+#import time
 
 def generate_launch_description():
 
