2be50db7f6aa1c1fef06c1bf7990a0cce1eba293
Soshi Saiki <sousaiky@yahoo.com>
2023-12-26 23:57:14 +0900

    add command

--- test/test.bash
+++ test/test.bash
@@ -3,9 +3,16 @@
 dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 
-cd $dir/ros2_ws
-colcon build
 source $dir/.bashrc
+
+ros2 interface show "person_msgs/srv/Query"
+
+cd $dir/ros2_ws
+
+colcon build
+
+source $dir/.bashrc
+
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
