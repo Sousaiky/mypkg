207be1fbeeda3469b70ed4bcf4d256b0becdb9bb
Soshi Saiki <sousaiky@yahoo.com>
2024-01-01 23:57:06 +0900

    change

--- test/test.bash
+++ test/test.bash
@@ -31,36 +31,8 @@ source $dir/.bashrc
 
 timeout 30 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-
-cat /tmp/mypkg.log |
-	grep '牡羊座'
-
-cat /tmp/mypkg.log |
-        grep '牡牛座'
-
-cat /tmp/mypkg.log |
-        grep '双子座'
-
-cat /tmp/mypkg.log |
-        grep '獅子座'
-
-cat /tmp/mypkg.log |
-        grep '乙女座'
-
-cat /tmp/mypkg.log |
-        grep '天秤座'
-
-cat /tmp/mypkg.log |
-        grep '蠍座'
-
-cat /tmp/mypkg.log |
-        grep '射手座'
-
-cat /tmp/mypkg.log |
-        grep '山羊座'
-
-cat /tmp/mypkg.log |
-        grep '水瓶座'
-
-cat /tmp/mypkg.log |
-        grep '魚座'
+if 321 <= request.birthday <= 331 and grep -q '牡羊座' /tmp/mypkg.log; then 
+	echo 0
+else 
+	echo 1
+fi
