322aa266f07ad99afb315dc77ea03c4f6a9126d0
Soshi Saiki <sousaiky@yahoo.com>
2024-01-02 23:10:47 +0900

    test test

--- test/test.bash
+++ test/test.bash
@@ -31,6 +31,16 @@ source $dir/.bashrc
 
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
+ng () {
+    echo "NG at Line $1"
+   res=1
+}
+
+res=0
+
 if grep -q '座' /tmp/mypkg.log && grep -q '今日の運勢が' /tmp/mypkg.log; then
-    echo 0
+    echo "OK"
+
+[ "$res" = 0 ] && echo "OK"
+exit $res
 fi
