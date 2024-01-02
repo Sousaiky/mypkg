f5235cdb69900c38a7a7e836ad5d9defc8a190ff
Soshi Saiki <sousaiky@yahoo.com>
2024-01-02 23:22:53 +0900

    rest test

--- test/test.bash
+++ test/test.bash
@@ -33,12 +33,12 @@ timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 ng () {
     echo "NG at Line $1"
-   res=1
+    res=1
 }
 
 res=0
 
-if grep -q '座' /tmp/mypkg.log && grep -q '今日の運勢が' /tmp/mypkg.log; then
+if grep -q 'e1' /tmp/mypkg.log || grep -q '今日の運勢が' /tmp/mypkg.log; then
     echo "OK"
 
 [ "$res" = 0 ] && echo "OK"
